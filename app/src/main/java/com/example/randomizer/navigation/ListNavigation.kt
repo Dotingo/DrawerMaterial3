f9bee19eb9913c0e46192b2312be47d3b8bc7f89
Dotingo <m9132824@gmail.com>
2024-08-19 13:32:14 +0300

    moving navigation to a separate function from InsideListScreen

--- app/src/main/java/com/example/randomizer/navigation/ListNavigation.kt
+++ app/src/main/java/com/example/randomizer/navigation/ListNavigation.kt
@@ -1,24 +1,52 @@
 package com.example.randomizer.navigation
 
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.randomizer.presentation.screens.lists.EditListScreen
 import com.example.randomizer.presentation.screens.lists.GroupListItemsScreen
+import com.example.randomizer.presentation.screens.lists.ListViewModel
 import com.example.randomizer.presentation.screens.lists.PickListItemsScreen
 import com.example.randomizer.presentation.screens.lists.ShuffleListItemsScreen
 
 @Composable
 fun ListNavigation(
     navController: NavHostController,
-    pickScreen: @Composable () -> Unit,
-    shuffleScreen: @Composable () -> Unit,
-    groupScreen: @Composable () -> Unit
+    paddingValues: PaddingValues,
+    splitItems: List<String>,
+    onBack: () -> Unit,
+    listViewModel: ListViewModel
 ) {
-    NavHost(navController, startDestination = NavigationItem.Pick.route) {
-        composable(NavigationItem.Pick.route) { pickScreen() }
-        composable(NavigationItem.Shuffle.route) { shuffleScreen() }
-        composable(NavigationItem.Group.route) { groupScreen() }
+    NavHost(navController, startDestination = NavigationItem.Pick.route,
+        enterTransition = {
+            EnterTransition.None
+        },
+        exitTransition = {
+            ExitTransition.None
+        },
+        popEnterTransition = {
+            EnterTransition.None
+        },
+        popExitTransition = {
+            ExitTransition.None
+        }
+    ) {
+        composable(NavigationItem.Pick.route) {
+            PickListItemsScreen(paddingValues, splitItems, listViewModel)
+        }
+        composable(NavigationItem.Shuffle.route) {
+            ShuffleListItemsScreen(paddingValues, splitItems, listViewModel)
+        }
+        composable(NavigationItem.Group.route) {
+            GroupListItemsScreen(paddingValues, splitItems, listViewModel)
+        }
+        composable("edit_screen") {
+            EditListScreen(navController, splitItems, onBack, listViewModel)
+        }
     }
 }
+
