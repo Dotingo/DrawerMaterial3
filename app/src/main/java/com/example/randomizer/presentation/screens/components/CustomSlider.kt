1afd6acc59b83142d3a8c80d52281269efa12e1f
Dotingo <m9132824@gmail.com>
2024-08-22 11:07:16 +0300

    moving slider to a separate function

---
+++ app/src/main/java/com/example/randomizer/presentation/screens/components/CustomSlider.kt
@@ -0,0 +1,38 @@
+package com.example.randomizer.presentation.screens.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.randomizer.R
+
+@Composable
+fun CustomSlider(valueRange: IntRange, currentValue: Int, onValueChange: (Int) -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "${stringResource(id = R.string.result_count)} $currentValue"
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 50.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "${valueRange.first}")
+            Slider(
+                value = currentValue.toFloat(),
+                onValueChange = { onValueChange(it.toInt()) },
+                valueRange = valueRange.first.toFloat()..valueRange.last.toFloat(),
+                modifier = Modifier.weight(1f),
+            )
+            Text(text = "${valueRange.last}")
+        }
+    }
+}
