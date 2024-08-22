8adf5f4c6937e30a60ec2584641dece2ec3a0016
Dotingo <m9132824@gmail.com>
2024-08-22 11:11:05 +0300

    create separate functions and improved code

--- app/src/main/java/com/example/randomizer/presentation/screens/coins/RandomCoinScreen.kt
+++ app/src/main/java/com/example/randomizer/presentation/screens/coins/RandomCoinScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -24,18 +23,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.LottieComposition
 import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieAnimationState
 import com.airbnb.lottie.compose.LottieClipSpec
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.randomizer.R
+import com.example.randomizer.presentation.screens.components.GenerateButton
 import com.example.randomizer.presentation.util.Dimens
-import com.example.randomizer.presentation.util.Dimens.MediumPadding1
 
 @Composable
 fun RandomCoinScreen(paddingValues: PaddingValues) {
@@ -43,17 +44,15 @@ fun RandomCoinScreen(paddingValues: PaddingValues) {
     var side by remember { mutableIntStateOf(1) }
     var isAnimating by remember { mutableStateOf(false) }
     val interactionSource = remember { MutableInteractionSource() }
+
     val animationState = animateLottieCompositionAsState(
         composition = composition,
         iterations = 5,
         speed = 15f,
         isPlaying = isAnimating,
-        clipSpec = if (side == 1) {
-            LottieClipSpec.Progress(0f, 1f)
-        } else {
-            LottieClipSpec.Progress(0f, 0.5f)
-        }
+        clipSpec = LottieClipSpec.Progress(0f, if (side == 1) 1f else 0.5f)
     )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -62,67 +61,85 @@ fun RandomCoinScreen(paddingValues: PaddingValues) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween,
     ) {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.8f)
-                .clickable(
-                    interactionSource = interactionSource,
-                    indication = null
-                ) {
-                    if (!isAnimating) {
-                        side = (1..2).random()
-                        isAnimating = true
-                    }
-                },
-            elevation = CardDefaults.cardElevation(2.dp)
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                LottieAnimation(
-                    composition = composition,
-                    progress = { animationState.progress },
-                    modifier = Modifier.scale(1.5f)
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(0.15f)
-                    .padding(end = Dimens.ExtraSmallPadding),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                if (!isAnimating) {
-                    Text(
-                        text = if (side == 1) {
-                            stringResource(id = R.string.head_coin)
-                        } else {
-                            stringResource(id = R.string.tail_coin)
-                        }, fontSize = 32.sp
-                    )
-                }
-            }
-        }
-        Button(modifier = Modifier
-            .padding(bottom = MediumPadding1),
-            onClick = {
+        CoinCard(
+            composition = composition,
+            animationState = animationState,
+            side = side,
+            isAnimating = isAnimating,
+            onCardClick = {
                 if (!isAnimating) {
                     side = (1..2).random()
                     isAnimating = true
                 }
-            }) {
-            Text(text = stringResource(id = R.string.toss_a_coin))
+            },
+            interactionSource = interactionSource
+        )
+
+        GenerateButton(label = stringResource(id = R.string.toss_a_coin), enabled = !isAnimating) {
+            if (!isAnimating) {
+                side = (1..2).random()
+                isAnimating = true
+            }
         }
 
         LaunchedEffect(animationState.isAtEnd && isAnimating) {
             if (animationState.isAtEnd) {
                 isAnimating = false
+            }
+        }
+    }
+}
+
+@Composable
+fun CoinCard(
+    composition: LottieComposition?,
+    animationState: LottieAnimationState,
+    side: Int,
+    isAnimating: Boolean,
+    onCardClick: () -> Unit,
+    interactionSource: MutableInteractionSource
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.8f)
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = onCardClick
+            ),
+        elevation = CardDefaults.cardElevation(2.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LottieAnimation(
+                composition = composition,
+                progress = { animationState.progress },
+                modifier = Modifier.scale(1.5f)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.15f)
+        ) {
+            if (!isAnimating) {
+                Text(
+                    text = if (side == 1) stringResource(id = R.string.head_coin) else stringResource(
+                        id = R.string.tail_coin
+                    ),
+                    fontSize = 32.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(0.15f)
+                        .padding(end = Dimens.ExtraSmallPadding),
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
