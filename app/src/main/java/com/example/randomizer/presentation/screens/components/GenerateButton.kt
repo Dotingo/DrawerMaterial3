5288bea0ba239fcb59d9fd8b3a78e238dbd4a6ba
Dotingo <m9132824@gmail.com>
2024-08-22 11:07:29 +0300

    moving button to a separate function

---
+++ app/src/main/java/com/example/randomizer/presentation/screens/components/GenerateButton.kt
@@ -0,0 +1,28 @@
+package com.example.randomizer.presentation.screens.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.randomizer.presentation.util.Dimens.MediumPadding1
+
+@Composable
+fun GenerateButton(
+    label: String,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        enabled = enabled,
+        modifier = modifier.padding(bottom = MediumPadding1)
+    ) {
+        Text(
+            text = label,
+            color = MaterialTheme.colorScheme.surface
+        )
+    }
+}
