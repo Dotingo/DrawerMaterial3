e7c9a6069535dfa04313e904fca5dd7b8f028b27
Dotingo <m9132824@gmail.com>
2024-08-22 11:06:35 +0300

    moving checkbox to a separate function

---
+++ app/src/main/java/com/example/randomizer/presentation/screens/components/CustomCheckbox.kt
@@ -0,0 +1,36 @@
+package com.example.randomizer.presentation.screens.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+import com.example.randomizer.presentation.util.Dimens.SmallPadding
+
+@Composable
+fun CustomCheckbox(checked: Boolean, text: String, onCheckedChange: (Boolean) -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .padding(horizontal = SmallPadding)
+            .toggleable(
+                value = checked,
+                onValueChange = onCheckedChange,
+                role = Role.Checkbox
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = text)
+        Checkbox(checked = checked, onCheckedChange = null)
+    }
+}
