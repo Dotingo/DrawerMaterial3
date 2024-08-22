baa7ec904504a10c346e5bef036bdd49af9a4c76
Dotingo <m9132824@gmail.com>
2024-08-22 11:13:21 +0300

    create separate function

---
+++ app/src/main/java/com/example/randomizer/presentation/screens/components/InfoScreen.kt
@@ -0,0 +1,43 @@
+package com.example.randomizer.presentation.screens.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.randomizer.R
+
+@Composable
+fun InfoScreen(paddingValues: PaddingValues = PaddingValues(0.dp), text: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_info),
+            contentDescription = "info",
+            modifier = Modifier.size(120.dp)
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        Text(
+            text = text,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.displayMedium
+        )
+    }
+}
