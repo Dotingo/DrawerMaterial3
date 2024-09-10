b6af1004cab49680944bb2134aba8816be9d3d9c
Dotingo <m9132824@gmail.com>
2024-09-10 20:05:11 +0300

    updated slider

--- app/src/main/java/com/example/randomizer/presentation/components/CustomSlider.kt
+++ app/src/main/java/com/example/randomizer/presentation/components/CustomSlider.kt
@@ -5,16 +5,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.randomizer.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomSlider(
     modifier: Modifier = Modifier,
@@ -38,6 +43,11 @@ fun CustomSlider(
                 value = currentValue.toFloat(),
                 onValueChange = { onValueChange(it.toInt()) },
                 valueRange = valueRange.first.toFloat()..valueRange.last.toFloat(),
+                thumb = {
+                    Icon(painter = painterResource(id = R.drawable.ic_dice),
+                        contentDescription = null,
+                        modifier = Modifier.size(26.dp))
+                },
                 modifier = Modifier.weight(1f),
             )
             Spacer(modifier = Modifier.width(3.dp))
